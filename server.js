bc25e31d58089cf9db4dc497d17903164741baad
Dominika Myszczynska <myszczynskad@gmail.com>
2019-06-11 20:10:40 +0100

    Connection to atlas

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://Dominikanam:<password>@cluster0-n0gtq.mongodb.net/test?retryWrites=true&w=majority', {
     useMongoClient: true
 });
 
